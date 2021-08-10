5532ccba786ca51e20e677ead3e4c7214a6a4ce8
sjd1997 <84625631+sjd1997@users.noreply.github.com>
2021-08-10 15:12:55 +0430

    Update blog.py

--- blog/blog.py
+++ blog/blog.py
@@ -17,7 +17,7 @@ bp = Blueprint("blog", __name__)
 @bp.route("/home/")
 def index():
     db = get_db()
-    posts = db.posts.find().sort({"date_created": -1})
+    posts = db.post.find().sort({"date_created": -1})
     for post in posts:
         post['_id'] = str(post['_id'])
     return render_template('blog/home.html', posts=posts)
@@ -26,7 +26,7 @@ def index():
 @bp.route("posts/<string:post_id>")
 def post_detail(post_id):
     db = get_db()
-    post = db.posts.find({"_id": ObjectId(post_id)})
+    post = db.post.find({"_id": ObjectId(post_id)})
 
     post['_id'] = str(post['_id'])
 
