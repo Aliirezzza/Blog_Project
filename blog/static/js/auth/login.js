0d922370b26749e2adffba4055a47b3d175f278c
Mostafa-N-E <84570342+Mostafa-N-E@users.noreply.github.com>
2021-08-11 17:40:11 +0430

    css and js of login and sign up template

---
+++ blog/static/js/auth/login.js
@@ -0,0 +1,18 @@
+
+// $(document).ready(function(){
+//
+// $('#smartwizard').smartWizard({
+// selected: 0,
+// theme: 'arrows',
+// autoAdjustHeight:true,
+// transitionEffect:'fade',
+// showStepURLhash: false,
+//
+// });
+//
+// });
+//
+// $('#SUBMIT').click(function(e){
+//   e.preventDefault();
+//   $('#register_form').submit();
+// })
