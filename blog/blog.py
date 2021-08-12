b60f144221a6565c44c739a0fee057c1782a54c7
Sajjad <zoghi_sajjad@yahoo.com>
2021-08-12 23:38:58 +0430

    blog.py edited

--- blog/blog.py
+++ blog/blog.py
@@ -1,3 +1,4 @@
+import pymongo
 from flask import Blueprint
 from flask import g
 from flask import flash
@@ -18,9 +19,13 @@ bp = Blueprint("blog", __name__)
 @bp.route("/home/")
 def index():
     db = get_db()
-    posts = db.post.find().sort({"create_date": -1})
+    posts = db.post.find().sort("create_date", pymongo.DESCENDING)
     for post in posts:
         post['_id'] = str(post['_id'])
+        print(post['_id'])
+        print(post['title'])
+        print(post['image'])
+        print(post['create_date'])
     return render_template('blog/home.html', posts=posts)
 
 
@@ -60,49 +65,49 @@ def create():
             db.post.insert_one({"title": title, "body": body, "category": category, "tag": tag, "image_url": image, "author_id": g.user["id"]})
             return redirect(url_for("blog.index"))
 
-    return render_template("blog/post.html")
+    return render_template("user/create_post.html")
 
 
-@bp.route("/<int:id>/update", methods=("GET", "POST"))
-@login_required
-def update(post_id):
-    """Update a post if the current user is the author."""
-
-
-    if request.method == "POST":
-        title = request.form["title"]
-        body = request.form["body"]
-        image = request.form["image"]
-        category = request.form["category"]
-        tag = request.form["tag"]
-        error = None
-
-        if not title:
-            error = "Title is required."
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.post.updateOne({
-                "_id" : post_id
-            }, {
-                    $set :{
-                    title: title,
-                    body : body,
-                    image_url : image,
-                    category : category,
-                    tag : tag
-                    }
-                })
-            # db.execute(
-            #     "UPDATE post SET title = ?, body = ?, image_url = ?, category = ?, tag = ?, WHERE id = ?",
-            #     (title, body, image, category, tag, id)
-            # )
-            # db.commit()
-            return redirect(url_for("blog.index"))
-
-    return render_template("blog/post.html", post=post)
+# @bp.route("/<int:id>/update", methods=("GET", "POST"))
+# @login_required
+# def update(post_id):
+#     """Update a post if the current user is the author."""
+#
+#
+#     if request.method == "POST":
+#         title = request.form["title"]
+#         body = request.form["body"]
+#         image = request.form["image"]
+#         category = request.form["category"]
+#         tag = request.form["tag"]
+#         error = None
+#
+#         if not title:
+#             error = "Title is required."
+#
+#         if error is not None:
+#             flash(error)
+#         else:
+#             db = get_db()
+#             db.post.updateOne({
+#                 "_id" : post_id
+#             }, {
+#                     $set :{
+#                     title: title,
+#                     body : body,
+#                     image_url : image,
+#                     category : category,
+#                     tag : tag
+#                     }
+#                 })
+#             # db.execute(
+#             #     "UPDATE post SET title = ?, body = ?, image_url = ?, category = ?, tag = ?, WHERE id = ?",
+#             #     (title, body, image, category, tag, id)
+#             # )
+#             # db.commit()
+#             return redirect(url_for("blog.index"))
+#
+#     return render_template("blog/post.html", post=post)
 
 
 @bp.route("/<int:id>/delete", methods=("POST",))
