8f8e9ca81017ddad6347b879186bd8706cd29305
Alireza Zare <84494667+Aliirezzza@users.noreply.github.com>
2021-08-19 20:01:33 +0430

    Delete db.py

--- blog/db.py
+++
@@ -1,8 +0,0 @@
-from flask import g
-from pymongo import MongoClient
-def get_db():
-    if "db" not in g:
-        client = MongoClient('mongodb://localhost:27017/')
-        g.db = client["blog"]
-
-    return g.db
